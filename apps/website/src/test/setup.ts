92436e0da35e77417ea30c2def6440ea10190e6f
CorrectRoadH <29306285+CorrectRoadH@users.noreply.github.com>
2026-03-23 18:49:16 +0800

    test: register website jest-dom matchers

--- apps/website/src/test/setup.ts
+++ apps/website/src/test/setup.ts
@@ -1,10 +1,7 @@
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
-if (typeof document !== "undefined") {
-  await import("@testing-library/jest-dom/vitest");
-  const { cleanup } = await import("@testing-library/react");
-
-  afterEach(() => {
-    cleanup();
-  });
-}
+afterEach(() => {
+  cleanup();
+});
